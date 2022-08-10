e57bec97dbf47eff0faf66e21e35d6b13e2f6a80
Ilnar Gabdullin <ilnar30012000@gmail.com>
2022-08-11 02:53:16 +0500

    added new autotest

---
+++ src/test/java/com/demoqa/tests/RegistrationFormWithPageObjectsTests.java
@@ -0,0 +1,68 @@
+package com.demoqa.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.demoqa.pages.RegistrationFormPage;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationFormWithPageObjectsTests {
+    RegistrationFormPage registrationFormPage = new RegistrationFormPage();
+
+    @BeforeAll
+    static void configure() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void fillFormTest() {
+
+        registrationFormPage.openPage()
+                            .setFirstName("Alexander")
+                            .setLastName("Ivanov")
+                            .setEmail("alexivanov22@mail.ru")
+                            .setGender("Male")
+                            .setNumber("8999888776")
+                            .setBirthDate("26","October","1970")
+                            .setSubjects("English")
+                            .setHobbies("Sports")
+                            .setPicture("nature.jpeg")
+                            .setCurrentAddress("Ufa")
+                            .setStateAndCity("NCR","Delhi")
+                            .setSubmit()
+
+                            .checkResultsModalIsVisible()
+                            .checkResult("Student Name","Alexander Ivanov")
+                            .checkResult("Student Email","alexivanov22@mail.ru")
+                            .checkResult("Gender","Male")
+                            .checkResult("Mobile","8999888776")
+                            .checkResult("Date of Birth","26 October,1970")
+                            .checkResult("Subjects","English")
+                            .checkResult("Hobbies","Sports")
+                            .checkResult("Picture","nature.jpeg")
+                            .checkResult("Address","Ufa")
+                            .checkResult("State and City","NCR Delhi");
+
+
+    } @Test
+    void fillFormWithMinimumDataTest() {
+
+        registrationFormPage.openPage()
+                .setFirstName("Alexander")
+                .setLastName("Ivanov")
+                .setGender("Male")
+                .setNumber("8999888776")
+                .setSubmit()
+
+                .checkResultsModalIsVisible()
+                .checkResult("Student Name","Alexander Ivanov")
+                .checkResult("Mobile","8999888776");
+
+    }
+}
