88c9c096fc1e8bafd34456692ae8f6f2eb05431b
Ilnar Gabdullin <ilnar30012000@gmail.com>
2022-08-11 03:12:06 +0500

    added new autotest

--- src/test/java/com/demoqa/pages/components/SelectLocationComponent.java
+++ src/test/java/com/demoqa/pages/components/SelectLocationComponent.java
@@ -7,6 +7,7 @@ import static com.codeborne.selenide.Selenide.$;
 public class SelectLocationComponent {
 
     public SelectLocationComponent setLocation(String state, String city) {
+
         $("#state").click();
         $("#stateCity-wrapper").$(byText(state)).click();
         $("#city").click();
